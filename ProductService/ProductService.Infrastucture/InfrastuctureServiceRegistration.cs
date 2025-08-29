06413c98ac880a1c1d8e6f20c359d24f049a0bbf
arcesantiago <arcesantiago90@gmail.com>
2025-08-29 11:58:17 -0300

    add domain add dbcontext add migrations

---
+++ ProductService/ProductService.Infrastucture/InfrastuctureServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService.Infrastucture.Percistence;
+
+namespace ProductService.Infrastucture
+{
+    public static class InfrastuctureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastuctureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddDbContext<ProductDbContext>(options =>
+            //options.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+            //);
+
+            return services;
+        }
+    }
+}
---
+++ ProductService/ProductService.Infrastucture/Percistence/ProductDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Domain;
+using ProductService.Domain.Common;
+
+namespace ProductService.Infrastucture.Percistence
+{
+    public class ProductDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;
+                Initial Catalog=Product; Integrated Security=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+        public DbSet<Product>? Products { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedAt = DateTime.Now;
+                        break;
+
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedAt = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
