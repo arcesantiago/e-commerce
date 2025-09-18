957c5f194662ced9628dc61a61f88ecf853f87d5
arcesantiago <arcesantiago90@gmail.com>
2025-09-18 08:21:24 -0300

    add dbcontext

---
+++ OrderService/OrderService/OrderService.Infrastructure/Percistence/OrderDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain;
+using OrderService.Domain.Common;
+
+namespace OrderService.Infrastructure.Percistence
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Order>? Products { get; set; }
+        public DbSet<OrderItem>? OrderItems { get; set; }
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedAt = DateTime.Now;
+                        break;
+
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedAt = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
